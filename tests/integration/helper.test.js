a87833777698149afd187f287a6e579e142c3ba4
Daniel Cousineau <dcousineau@gmail.com>
2021-07-16 16:30:36 -0400

    Add CI, CODEOWNERS, update deps

--- tests/integration/helper.test.js
+++ tests/integration/helper.test.js
@@ -2,7 +2,7 @@ const { run, TARTUFO_HELPER } = require("./util");
 
 describe("bin/tartufo-helper", () => {
   test("Run tartufo-helper doctor", async () => {
-    const [stdout, stderr] = await run(TARTUFO_HELPER, ["doctor"]);
+    const [stdout] = await run(TARTUFO_HELPER, ["doctor"]);
     expect(stdout).toEqual(expect.stringMatching(/Your system is ready to use Tartufo!/i));
   });
 });
--- tests/integration/shim.test.js
+++ tests/integration/shim.test.js
@@ -2,7 +2,7 @@ const { run, TARTUFO_SHIM } = require("./util");
 
 describe("bin/tartufo-shim", () => {
   test("Can get tartufo version", async () => {
-    const [stdout, stderr] = await run(TARTUFO_SHIM, ["--version"]);
+    const [stdout] = await run(TARTUFO_SHIM, ["--version"]);
     expect(stdout).toEqual(expect.stringMatching(/^tartufo, version [0-9]+\.[0-9]+\.[0-9]+$/i));
   });
 });
